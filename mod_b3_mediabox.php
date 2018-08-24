5875a14fce541e12d06fa938d22985ff314ec031
Hugo Fittipaldi <hugo.fittipaldi@gmail.com>
2018-08-24 10:51:22 -0300

    Media files renamed and created another style sheet with the css extracted from the module main file

--- mod_b3_mediabox.php
+++ mod_b3_mediabox.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  mod_b3_mediabox
  *
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -21,9 +21,10 @@ if ($params->def('prepare_content', 1))
 
 if (!$params->def('bootstrap', 1))
 {
-    JHtml::_('stylesheet', 'mod_b3_mediabox/mod_b3_mediabox.css', ['relative' => true]);
-    JHtml::_('script', 'mod_b3_mediabox/mod_b3_mediabox.js', ['relative' => true]);
+    JHtml::_('stylesheet', 'mod_b3_mediabox/mod_b3_modal.css', ['relative' => true]);
+    JHtml::_('script', 'mod_b3_mediabox/mod_b3_modal.js', ['relative' => true]);
 }
+JHtml::_('stylesheet', 'mod_b3_mediabox/mod_b3_mediabox.css', ['relative' => true]);
 
 $width   = $params->get('width', 800) + 2;
 $height  = $params->get('height', 800) + 2;
--- tmpl/default.php
+++ tmpl/default.php
@@ -3,43 +3,23 @@
  * @package     Joomla.Site
  * @subpackage  mod_b3_mediabox
  *
- * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
-$modal         = '.modal-open .modal { display: -webkit-box !important; display: -ms-flexbox !important; display: flex !important; -webkit-box-pack: center; -ms-flex-pack: center; justify-content: center; }';
-$modal_dialog  = '#b3Mediabox .modal-dialog { width: ' . $width . 'px; max-width: calc(100vw - 20px); }';
-$modal_content = '#b3Mediabox .modal-content { overflow: hidden; }';
+$modal_dialog  = '#b3Mediabox .modal-dialog { width: ' . $width . 'px; }';
+$modal_content = '#b3Mediabox .modal-content { height: ' . $height . 'px; }';
 $modal_body    = '#b3Mediabox .modal-body { padding: ' . $padding . 'px; }';
 
 $padding += 5;
 $modal_close = '#b3Mediabox button.close {
-    display: -webkit-box;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-pack: center;
-        -ms-flex-pack: center;
-            justify-content: center;
-    -webkit-box-align: center;
-        -ms-flex-align: center;
-            align-items: center;
-    position: absolute;
-    right: 0;
     margin-top: ' . $padding . 'px;
     margin-right: ' . $padding . 'px;
-    border-radius: 3px;
-    width: 20px;
-    height: 20px;
-    padding-left: 1px;
-    line-height: 20px;
-    font-family: sans-serif;
-    background-color: #fff;
 }';
 
-$style  = $modal . $modal_dialog . $modal_content . $modal_body . $modal_close;
-$style .= "@media (max-width: 991px) { body.modal-open { padding-right: 0!important; overflow: visible; } }\n";
+$style  = $modal_dialog . $modal_content . $modal_body . $modal_close;
 
 $doc->addStyleDeclaration($style);
 $doc->addScriptDeclaration("
@@ -54,8 +34,8 @@ $doc->addScriptDeclaration("
 <div class="modal fade hidden-xs hidden-sm" id="b3Mediabox">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-body">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <div class="modal-body">
                 <?php echo $module->content; ?>
 
             </div>
